539a1c97f79503b14e9e913fd93f6c42bb5dede7
correiap <pedro.lcorreia@gmail.com>
2018-09-13 10:08:13 -0400

    Add files via upload

---
+++ suno.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Set id to start from"
+read n
+echo "set last id"
+read x
+echo "Enter the report number"
+read sc
+mkdir /home/ec2-user/suno/sc$sc
+# continue until $n equals 308636
+while [ $n -le $x ]
+do
+        echo "Downloading ID #$n out of $x"
+        wget -P /home/ec2-user/suno/sc$sc https://membros.sunoresearch.com.br/download/clube-small-caps-$sc/?wpdmdl=$n
+        n=$(( n+1 ))     # increments $n
+done
