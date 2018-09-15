c823d43520fb6e792413643434382406b7d1033d
Pedro Correia <pedro.lcorreia@gmail.com>
2018-09-14 20:18:07 -0400

    modified script to delete non-report files

--- suno.sh
+++ suno.sh
@@ -12,9 +12,25 @@ read sc
 echo " Create directory to save reports"
 mkdir /home/ec2-user/suno/sc$sc
 
+#minimum report size in bytes
+minfilesize=5000
+
+echo "Script running..."
+
 while [ $n -le $x ]
 do
         #echo "Downloading ID #$n out of $x"
-        wget -P /home/ec2-user/suno/sc$sc https://membros.sunoresearch.com.br/download/clube-small-caps-$sc/?wpdmdl=$n
-        n=$(( n+1 ))     
+        wget -q -P /home/ec2-user/suno/sc$sc https://membros.sunoresearch.com.br/download/clube-small-caps-$sc/?wpdmdl=$n
+	
+	#get filesize
+        filesize=$(wc -c <"/home/ec2-user/suno/sc$sc/index.html?wpdmdl=$n")
+        
+	if [ $filesize -gt $minfilesize ]; then
+                echo " Small Caps Report found and the size is: " $filesize
+        else
+                #echo "file should be deleted"
+		rm -f /home/ec2-user/suno/sc$sc/index.html?wpdmdl=$n
+        fi
+	
+	n=$(( n+1 ))
 done
